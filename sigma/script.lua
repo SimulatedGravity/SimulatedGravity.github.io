da669ec94b9ef72e2db6cf80400f0912ecfafe12
SimulatedGravity <nielsabel08@gmail.com>
2024-06-08 19:40:48 +1200

    Update script.lua

--- sigma/script.lua
+++ sigma/script.lua
@@ -1,9 +1,5 @@
-print("this is a certified hood classic");
+print("test1")
 
-local h1 = get("h1");
-print("and the code did not crash");
-h1.set_contents("0");
-
--- get("button").on_click(function()
---		h1.set_contents(tostring(math.random(1,100)))
--- end)
+local h1 = get("h1")
+print("test2")
+h1.set_contents("0")
