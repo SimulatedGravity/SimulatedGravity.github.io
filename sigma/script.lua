994917e555785e5fca8c03985e98b03fe80bf859
SimulatedGravity <nielsabel08@gmail.com>
2024-06-08 17:24:20 +1200

    Update script.lua

--- sigma/script.lua
+++ sigma/script.lua
@@ -1,6 +1,6 @@
 local h1 = get("h1")
 h1.set_contents("0")
 
-get("button").on_click(function()
-	h1.set_contents(tostring(math.random(1,100)))
-end)
+!-- get("button").on_click(function()
+!--		h1.set_contents(tostring(math.random(1,100)))
+!-- end)
