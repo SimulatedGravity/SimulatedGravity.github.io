2a650b794b3be00a655086cb85346b4c17dce17e
SimulatedGravity <107592310+SimulatedGravity@users.noreply.github.com>
2026-04-10 16:56:36 +1200

    ok deleted stuff and added magic box

--- sigma/random-gen/script.lua
+++
@@ -1,6 +0,0 @@
-local h1 = get("header")
-h1.set_content("0")
-
-get("btn").on_click(function()
-	h1.set_content(tostring(math.random(1,100)))
-end)
--- sigma/script.lua
+++
@@ -1,5 +0,0 @@
-local response = get("response")
-
-get("btn").on_click(function()
-	response.set_content("nice try, you dont have admin rights")
-end)
